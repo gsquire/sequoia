a30bc7ce23a0eaa22bcf22895173f7d2b2c590d0
Garrett Squire <garrettsquire@gmail.com>
2018-07-04 12:08:23 -0700

    add a benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,24 @@
+#[macro_use]
+extern crate criterion;
+extern crate sequoia;
+
+use criterion::Criterion;
+use sequoia::json_encoder::JsonEncoder;
+use sequoia::{Entry, Level, Logger};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut l = Logger::new(::std::io::sink());
+    c.bench_function("simple log", move |b| {
+        b.iter(|| {
+            l.log(
+                Entry::new(Some(Level::Info), JsonEncoder)
+                    .str("key", "value")
+                    .integer("integer", 7)
+                    .msg("top level message"),
+            )
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
